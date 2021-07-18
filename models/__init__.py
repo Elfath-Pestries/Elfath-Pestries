f2ab38d2a352f20fff7bcc8af17d0e7024b668e8
new setup <numeric1int23@gmail.com>
2021-07-18 17:55:14 +0200

    READOME

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import pos_config
-from . import pos_delivery_order
-from . import pos_order
-from . import stock
--- models/pos_config.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class PosConfigInherit(models.Model):
-    _inherit = "pos.config"
-
-    home_delivery = fields.Boolean('POS Home Delivery',default=False)
